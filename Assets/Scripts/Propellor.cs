ff1e7d5fcfc019956488209a3f466c96d48b9d17
YotamBenda <59575705+YotamBenda@users.noreply.github.com>
2020-07-17 13:30:04 +0300

    Propellor

---
+++ Assets/Scripts/Propellor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propellor : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 200f) * Time.deltaTime);
+    }
+
+}
