a317bc11abccad2aecf81ecddde0c1c34ca1457a
YotamBenda <59575705+YotamBenda@users.noreply.github.com>
2020-07-17 10:47:46 +0300

    Base project
    asdasdasd

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject[] ball;
+    public Transform spawnPoint;
+    private Vector3 spawnVec;
+
+    private int currBall = 0;
+    [SerializeField]
+    private int force = 100;
+
+    void Start()
+    {
+        spawnVec = spawnPoint.transform.position;
+        StartCoroutine(Spawn());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator Spawn()
+    {
+        for(int i =0; i<10; i++)
+        {
+            var ballToSpawn = Instantiate(ball[currBall], spawnVec, Quaternion.identity) as GameObject;
+            ballToSpawn.GetComponent<Rigidbody>().AddForce(Vector3.forward * force, ForceMode.Acceleration);
+            if(currBall < 3)
+            {
+                currBall++;
+            }
+            else
+            {
+                currBall = 0;
+            }
+            Debug.Log(currBall);
+            Debug.Log(ball.Length);
+            yield return new WaitForSeconds(4);
+        }
+        
+
+    }
+}
