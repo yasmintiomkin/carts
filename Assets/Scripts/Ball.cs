9fa89ff68299d9382b6b489467ca361787457fbc
yasmin <yasmin.tiomkin@gmail.com>
2020-07-17 13:20:26 +0300

    ball script

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ball : MonoBehaviour
 {
-    
+    SphereCollider sphereCollider;
+    float bouncinessOrig;
+    float dynamicFrictionOrig;
+
+    public float powerUpBouncinessScale = 2;
+    public float powerUpDynamicFrictionScale = 2;
 
     void Start()
     {
-        
+        sphereCollider = GetComponent<SphereCollider>();
+        bouncinessOrig = sphereCollider.material.bounciness;
+        dynamicFrictionOrig = sphereCollider.material.dynamicFriction;
     }
 
     void Update()
     {
         
     }
+
+    public void PowerUp(bool on)
+    {
+        if (on)
+        {
+            sphereCollider.material.bounciness = bouncinessOrig * powerUpBouncinessScale;
+            sphereCollider.material.dynamicFriction = dynamicFrictionOrig * powerUpDynamicFrictionScale;
+        }
+        else
+        {
+            sphereCollider.material.bounciness = bouncinessOrig;
+            sphereCollider.material.dynamicFriction = dynamicFrictionOrig;
+        }
+    }
+
 }
