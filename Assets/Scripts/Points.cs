730d1e445a3c7cdcc4474d552f3ef7f7bc4c5879
YotamBenda <59575705+YotamBenda@users.noreply.github.com>
2020-07-18 21:59:15 +0300

    Level Design
    Added PointsGates

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Points : MonoBehaviour
 {
     [SerializeField] int score;
     [SerializeField] GameManager gameManager;
+    [SerializeField] Text textOnGates;
+
+    private void Start()
+    {
+        textOnGates.text = score.ToString();
+        SetTextColor();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,6 +26,25 @@ public class Points : MonoBehaviour
         if (gameManager == null)
         {
             return;
+        }
+    }
+    void SetTextColor()
+    {
+        if (score <= 100)
+        {
+            textOnGates.color = Color.red;
+        }
+        else if(score >100 && score <= 250)
+        {
+            textOnGates.color = Color.blue;
+        }
+        else if(score >250 && score <= 450)
+        {
+            textOnGates.color = Color.green;
+        }
+        else if (score > 450)
+        {
+            textOnGates.color = Color.Lerp(Color.yellow, Color.cyan, 1f);
         }
     }
 }
